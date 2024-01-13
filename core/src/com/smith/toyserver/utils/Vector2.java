aecc1db58b3c959bb35282e8a3d60372b70b0313
DeveloperTruthStare <127274792+DeveloperTruthStare@users.noreply.github.com>
2024-01-13 03:56:03 -0800

    made vector2 class serializable

--- core/src/com/smith/toyserver/utils/Vector2.java
+++ core/src/com/smith/toyserver/utils/Vector2.java
@@ -3,7 +3,10 @@ package com.smith.toyserver.utils;
 public class Vector2 {
     public float x;
     public float y;
-
+    public Vector2() {
+        x = 0;
+        y = 0;
+    }
     public Vector2(float x, float y) {
         this.x = x;
         this.y = y;
